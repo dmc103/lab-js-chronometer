2d98451cf310feba92c97b139ed98bf4084c1ee5
Doreen <doreenmarie_cee@hotmail.com>
2023-09-09 17:30:26 +0200

    completing all iterations

--- src/chronometer.js
+++ src/chronometer.js
@@ -22,21 +22,36 @@ class Chronometer {
 
   getSeconds() {
     // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    return value.toString().padStart(2, '0');
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0;
+    if (this.intervalId !== null) {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    }
   }
 
   split() {
     // ... your code goes here
+    const minutes = this.getMinutes();
+    const seconds = this.getSeconds();
+
+    const formatMinutes = this.computeTwoDigitNumber(minutes);
+    const formatSeconds = this.computeTwoDigitNumber(seconds);
+
+    return `${formatMinutes}:${formatSeconds}`;
   }
 }
