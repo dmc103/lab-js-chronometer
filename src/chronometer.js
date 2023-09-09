a2074a7bfd025e3b235155c2125ddc65993d96bb
Doreen <doreenmarie_cee@hotmail.com>
2023-09-09 16:52:51 +0200

    iteration 1 to 3

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,14 +1,23 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    this.intervalId = setInterval (() => {
+      this.currentTime += 1;
+      if (printTimeCallback) {
+        printTimeCallback ()
+      }
+    }, 1000);
   }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor (this.currentTime / 60);
   }
 
   getSeconds() {
